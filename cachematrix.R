382415b9640209b13f1553e93e99872df85cc701
aron.patrick@ky.gov <aron.patrick@ky.gov>
2015-11-22 18:44:40 -0500

    cachematrix.R
    Inverting matrices can be computationally time-consuming. In order to
    reduce computing time, if the inverted matrix is stored in the cache, so
    that if already solved for, it can be retrieved rather than

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Inverting matrices can be computationally time-consuming. In order to reduce computing time, 
+## if the inverted matrix is stored in the cache, so that if already solved for, it can be retrieved rather than 
+
+
+## The purpose of this function is twofold.
+## First, to calculate the inverse of the matrix passed to it.
+## Second, if the inverse of the matrix has already been calcuated, it is retrieved from the cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   inv_x <- NULL
+   set <- function(y) {
+     x <<- y
+     inv_x <<- NULL
+   }
+   get <- function() x
+   setinverse<- function(inverse) inv_x <<-inverse
+   getinverse <- function() inv_x
+   list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+## This function below, cacheSolve, returns the matrix created above if available. If not it solves it. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+   inv_x <- x$getinverse()
+   if (!is.null(inv_x)) {
+     return(inv_x)
+   } else {
+     inv_x <- solve(x$get())
+     x$setinverse(inv_x)
+     return(inv_x)
+   }
 }
